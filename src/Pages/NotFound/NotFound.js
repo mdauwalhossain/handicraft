79d9e687c1b485fc40cbb00b41699e9808d62ba8
Md Auwal Hossain <mdauwalhossain2020@gmail.com>
2021-11-22 09:33:24 +0600

    Not Found

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1>404 || NOT FOUND</h1>
+            <h1>NOT FOUND</h1>
             <img src="https://cdn4.vectorstock.com/i/1000x1000/85/43/error-page-not-found-vector-27898543.jpg" alt=""></img>
         </div>
     );
